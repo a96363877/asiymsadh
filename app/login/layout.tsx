49a7618cf67cdeac8b4e52edf95244eee4499ddf
a96363877 <a96363877@gmail.com>
2025-05-09 09:48:44 +0300

    01

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="ar" dir="rtl">
+    <html lang="ar" >
       <body className={inter.className}>{children}</body>
     </html>
   )
